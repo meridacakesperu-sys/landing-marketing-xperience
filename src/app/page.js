9e2c2c3d35a49685ca04b02e9f7ba305015df7c6
Eduar Peña <eduarantoniopenasosa@Eduars-MacBook-Air.local>
2026-09-05 12:25:08 -0400

    Remove descriptions from unrevealed speakers

--- src/app/page.js
+++ src/app/page.js
@@ -544,7 +544,6 @@ export default function Home() {
               <div className="speaker-overlay-gradient"></div>
               <div className="speaker-info-gapless">
                 <h3>Próximamente será revelado</h3>
-                <p>Ventas online y offline. Experto en escalar negocios a través de estrategias de venta digital y retail tradicional</p>
               </div>
             </div>
 
@@ -553,7 +552,6 @@ export default function Home() {
               <div className="speaker-overlay-gradient"></div>
               <div className="speaker-info-gapless">
                 <h3>Próximamente será revelado</h3>
-                <p>Comunicadora social, creadora de contenido especialista en comunicación y negocios con marcas a nivel nacional</p>
               </div>
             </div>
 
